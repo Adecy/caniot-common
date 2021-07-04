cca3f369198e4ac00e4665f99e43121855e6a282
Lucas Dietrich <ld.adecy@gmail.com>
2021-07-04 11:28:38 +0200

    fix read address (16b) from PROGMEM in attributes resolve

--- caniot/attributes.cpp
+++ caniot/attributes.cpp
@@ -15,7 +15,7 @@ const uint8_t Attributes::resolve(const key_t key, attr_ref_t *const p_attr_ref)
 
         if (ATTR_KEY_ATTR(key) < (uint8_t) pgm_read_byte(&section_p->array_size))
         {
-            const attribute_t * const attr_array_p = (const attribute_t *) pgm_read_byte(&section_p->array);    // read attr array adress from PROGMEM
+            const attribute_t * const attr_array_p = (const attribute_t *) pgm_read_word(&section_p->array);    // read attr array adress from PROGMEM
             const attribute_t * const attr_p = &attr_array_p[ATTR_KEY_ATTR(key)];                               // get attr address in PEOGMEM
             const uint8_t attr_size = (uint8_t) pgm_read_byte(&attr_p->size);
 
